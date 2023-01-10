7aa7ba17f827b61cb38b1edf6d155e91bb116b72
james li <hello@gmail.com>
2023-01-10 14:41:11 +0800

    first commit

---
+++ src/com/sun/HelloGit.java
@@ -0,0 +1,12 @@
+package com.sun;
+
+/**
+ * Author: James
+ * Date: 2023/1/10
+ * Time: 14:32
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
