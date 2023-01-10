24ba033e99009e1f610fcabbd431b91fb3cc368e
james li <hello@gmail.com>
2023-01-10 14:52:54 +0800

    add Test

---
+++ src/com/sun/Test.java
@@ -0,0 +1,12 @@
+package com.sun;
+
+/**
+ * Author: James
+ * Date: 2023/1/10
+ * Time: 14:52
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
